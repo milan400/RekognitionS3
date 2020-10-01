63653d3792e1d900d2f40f91ec7494cde66eda7c
milan <tripathimilan28@gmail.com>
2020-10-01 19:37:21 +0545

    Update app.py

--- app.py
+++ app.py
@@ -94,5 +94,5 @@ def uploader():
         
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
-    #app.run(debug = True)
+    #app.run(host='0.0.0.0', port=8080)
+    app.run(debug = True)
